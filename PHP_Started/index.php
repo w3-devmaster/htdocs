593fadeab2b2c07444e9f17a2571ed42e254087c
w3-devmaster <apichat_pep@hotmail.com>
2023-04-01 22:19:59 +0700

    01-04-2566

---
+++ PHP_Started/index.php
@@ -0,0 +1,51 @@
+<?php
+
+// $a = 1;
+// $b = '1';
+
+// var_dump( $a === $b );
+
+// $firstname = 'apichat';
+
+// $firstname .= ' kumjensi';
+
+// echo $firstname;
+
+// define( 'AGE', 33 );
+
+// echo AGE;
+// $age = 33; //
+
+// $age = $age + 1; // $age ++ , $age += 1
+// $age = 40;
+
+// echo $age ** 5;
+
+// $x = 10;
+// $z = 10.50; // Float -- Double
+
+// $is_admin = true; // Boolean
+// $a        = 1; // Integer
+
+// $_name = 'apichat kumjensi';
+// $Name  = 'PEP';
+
+// $foot_smail = null;
+
+// $foot_smail_txt = '';
+
+// echo strlen( $is_admin );
+
+// $a = 10;
+
+// $b = 18;
+
+// $c = 19;
+
+// $d = 18;
+// // false             true
+// $adult = ( $d > 18 || $c == 19 );
+
+// var_dump( $adult );
+
+?>
